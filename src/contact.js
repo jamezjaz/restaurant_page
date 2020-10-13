a086464931df2eff8dd81418823efd2a833f1e78
Jamezjaz <jamezjaz@gmail.com>
2020-10-13 12:41:22 -0500

    docs: modifies the contact form page

--- src/contact.js
+++ src/contact.js
@@ -19,20 +19,24 @@ const contactMe = (() =>{
                                     <p>Place Your Order Now</p>
                                 </div>
                                 <form>
-                                    <div class="form-group py-3">
+                                    <div class="form-group py-2">
                                         <label for="exampleInputEmail1">Email address</label>
                                         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                                     </div>
-                                    <div class="form-group py-3">
+                                    <div class="form-group py-2">
                                         <label for="exampleInputPassword1">Password</label>
                                         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                                     </div>
-                                    <div class="form-check py-3">
+                                    <div class="form-group">
+                                        <label for="exampleFormControlTextarea1">Message</label>
+                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                    </div>
+                                    <div class="form-check py-2">
                                         <input type="checkbox" class="form-check-input" id="exampleCheck1">
                                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
                                     </div>
-                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                    <button type="submit" class="btn btn-primary">Send A Message</button>
                                 </form>
                               </div>
                               `;
