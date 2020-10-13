f614519a1c4ed4148153a1e44ee71aaf1d5b9daf
Jamezjaz <jamezjaz@gmail.com>
2020-10-13 08:49:40 -0500

    docs: implements bg changer function

---
+++ src/bgChanger.js
@@ -0,0 +1,11 @@
+function bgChanger() {
+    if(this.scrollY > this.innerHeight / 10) {
+      document.body.classList.add('mainmenu');
+    } else {
+      document.body.classList.remove('mainmenu');
+    }
+  }
+  
+  window.addEventListener('scroll', bgChanger);
+
+  export default bgChanger;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import navBar from './navbar';
 import footer from './footer';
 import menu from './menu';
+import bgChanger from './bgChanger';
 
 const index = (() =>{
   const content = document.querySelector('#content');
   const home = document.querySelector('.home');
   const welcomeMsg = document.createElement('div');
+  const mainMenu = document.querySelector('#main-menu');
 
+  mainMenu.classList.add('mainmenu');
   home.classList.add('bgimage');
   welcomeMsg.classList = 'text-white font-weight-bold bg-danger rounded w-50 p-4 ml-4'
 
