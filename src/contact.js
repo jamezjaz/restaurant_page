8bb2c585888483790cd2af4f4cf532c8c0fa36ca
Jamezjaz <jamezjaz@gmail.com>
2020-10-14 03:29:30 -0500

    docs: validates contact form

--- src/contact.js
+++ src/contact.js
@@ -21,16 +21,16 @@ const contactMe = (() =>{
                                 <form>
                                     <div class="form-group py-2">
                                         <label for="exampleInputEmail1">Email address</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required>
                                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                                     </div>
                                     <div class="form-group py-2">
                                         <label for="exampleInputPassword1">Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
                                     </div>
                                     <div class="form-group">
                                         <label for="exampleFormControlTextarea1">Message</label>
-                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" required></textarea>
                                     </div>
                                     <div class="form-check py-2">
                                         <input type="checkbox" class="form-check-input" id="exampleCheck1">
